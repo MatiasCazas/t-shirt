75664b64136d62fd2094f7a6529b7440487b2440
MCazas <mati.cazas99@gmail.com>
2024-04-17 16:32:28 -0300

    checkout basico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { SliderComponent } from './slider/slider.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SliderComponent],
+  imports: [RouterOutlet, HeaderComponent, SliderComponent, CheckoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
