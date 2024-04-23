508d65d7ec94c47861bbedef8ef6e3dbe7fecff2
santoslinares1 <santos9linares@gmail.com>
2024-04-23 12:56:24 -0300

    Footer, header, message and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { MessageComponent } from './message/message.component';
+import { FooterComponent } from './footer/footer.component';
 
 export const routes: Routes = [
-    {path: "checkout", component: CheckoutComponent}
+    {path: "checkout", component: CheckoutComponent},
+    {path: "message", component: MessageComponent},
+    {path: "footer", component: FooterComponent}
+    
 ];
