68faaffee160a3da406e9b62e3b08bd69b1a6f4f
MCazas <mati.cazas99@gmail.com>
2024-04-23 19:11:09 -0300

    principio de customizacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,19 @@
 import { Routes } from '@angular/router';
 import { CheckoutComponent } from './checkout/checkout.component';
+import { DesignComponent } from './design/design.component';
+import { CustomizationComponent } from './customization/customization.component';
 
 export const routes: Routes = [
-    {path: "checkout", component: CheckoutComponent}
+    { 
+        path: "checkout", 
+        component: CheckoutComponent
+    },
+    { 
+        path: "design",
+        component: DesignComponent,
+    },
+    { 
+        path: "customize",
+        component: CustomizationComponent,
+    },
 ];
