bf83769540e9571e621cb0aac5637ef4b7f0eea1
santoslinares1 <santos9linares@gmail.com>
2024-05-17 17:14:47 -0300

    modificaciones

--- src/app/egresados/egresados.component.ts
+++ src/app/egresados/egresados.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-egresados',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './egresados.component.html',
   styleUrl: './egresados.component.css'
 })
 export class EgresadosComponent {
+  images: string[] = ['assets/Egresados/slide-egresados1.png','assets/slide2.jpg', 'assets/slide3.jpg'];
+  currentIndex = 0;
+  translateValue = 0;
 
+  constructor() { 
+
+  }
+
+  ngOnInit(): void {
+  }
+
+
+  //slider
+  prevSlide(): void {
+    if (this.currentIndex === 0) {
+      this.currentIndex = this.images.length - 1;
+    } else {
+      this.currentIndex--;
+    }
+    this.updateSlider();
+  }
+
+  nextSlide(): void {
+    if (this.currentIndex === this.images.length - 1) {
+      this.currentIndex = 0;
+    } else {
+      this.currentIndex++;
+    }
+    this.updateSlider();
+  }
+
+  updateSlider(): void {
+    this.translateValue = -100 * this.currentIndex;
+  }
 }
--- src/app/kids/kids.component.ts
+++ src/app/kids/kids.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-kids',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './kids.component.html',
   styleUrl: './kids.component.css'
 })
 export class KidsComponent {
+  images: string[] = ['assets/Kids/slide-diseno1.png','assets/Kids/slide-diseno2.png', 'assets/Kids/slide-diseno3.png', 'assets/Kids/slide-diseno4.png', 'assets/Kids/slide-diseno5.png'];
+  currentIndex = 0;
+  translateValue = 0;
 
+  constructor() { 
+
+  }
+
+  ngOnInit(): void {
+  }
+
+
+  //slider
+  prevSlide(): void {
+    if (this.currentIndex === 0) {
+      this.currentIndex = this.images.length - 1;
+    } else {
+      this.currentIndex--;
+    }
+    this.updateSlider();
+  }
+
+  nextSlide(): void {
+    if (this.currentIndex === this.images.length - 1) {
+      this.currentIndex = 0;
+    } else {
+      this.currentIndex++;
+    }
+    this.updateSlider();
+  }
+
+  updateSlider(): void {
+    this.translateValue = -100 * this.currentIndex;
+  }
 }
